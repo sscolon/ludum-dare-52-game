68c5625843531874f542de3171966c4a2e6ae1c0
Scolon <49799704+sscolon@users.noreply.github.com>
2023-01-06 18:32:51 -0500

    Implemented Sprite Outline Code, we're going to need to create a shader later.

---
+++ Assets/Scripts/Graphics/SpriteOutline.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Mechanizer
+{
+    public class SpriteOutline : MonoBehaviour
+    {
+        private SpriteRenderer _target;
+        [SerializeField] private SpriteRenderer[] _renderers;
+        private void UpdateSprite()
+        {
+            if (_target == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < _renderers.Length; i++)
+            {
+                var renderer = _renderers[i];
+                renderer.sprite = _target.sprite;
+            }
+        }
+
+        private void UpdateOutline()
+        {
+            if (_target == null)
+            {
+                return;
+            }
+
+            transform.position = _target.transform.position;
+        }
+
+        public void Outline(SpriteRenderer target)
+        {
+            _target = target;
+        }
+
+        private void LateUpdate()
+        {
+            UpdateOutline();
+            UpdateSprite();
+        }
+    }
+}
