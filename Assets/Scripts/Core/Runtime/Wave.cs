8b4cd09a4c1eb2474841f401384a6ba9f454c1cb
Scolon <49799704+sscolon@users.noreply.github.com>
2023-01-06 20:03:39 -0500

    Create Meta Data

--- Assets/Scripts/Core/Runtime/Wave.cs
+++ Assets/Scripts/Core/Runtime/Wave.cs
@@ -29,7 +29,14 @@ namespace Mechanizer
 
         public int GetSpawnAtPosition(Vector2 position)
         {
+            for(int i = 0; i < spawns.Count; i++)
+            {
+                var spawn = spawns[i];
+                if (spawn.position == position)
+                    return i;
+            }
 
+            return -1;
         }
     }
 }
