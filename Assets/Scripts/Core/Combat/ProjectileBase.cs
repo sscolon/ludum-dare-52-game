f695379b679207e39ce505faaa11fe0879c9b71d
Scolon <49799704+sscolon@users.noreply.github.com>
2023-01-06 20:00:59 -0500

    Added Projectile Move Foward, a basic projectile type.

--- Assets/Scripts/Core/Combat/ProjectileBase.cs
+++ Assets/Scripts/Core/Combat/ProjectileBase.cs
@@ -5,12 +5,16 @@ namespace Mechanizer
 {
     public abstract class ProjectileBase : Attack
     {
+        public Vector2 Origin { get; set; }
+        public Vector2 Target { get; set; }
+
         public event Action OnDeath;
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (DoDamage(collision))
             {
                 OnDeath?.Invoke();
+                Destroy(gameObject);
             }
         }
     }
---
+++ Assets/Scripts/Core/Combat/ProjectileMoveForward.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Mechanizer
+{
+    public class ProjectileMoveForward : ProjectileBase
+    {
+        [SerializeField] private Rigidbody2D _projectileBody;
+        [SerializeField] private float _movementSpeed;
+        public float MovementSpeed { get => _movementSpeed; set => _movementSpeed = value; }
+        private void OnValidate()
+        {
+            if (_projectileBody == null)
+                _projectileBody = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 direction = (Origin - Target).normalized;
+            Vector2 velocity = direction * MovementSpeed;
+            _projectileBody.velocity = velocity;
+        }
+    }
+}
