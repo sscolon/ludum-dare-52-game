c207dce0d007f9dcbfaf392f4f01856d2f974162
Scolon <49799704+sscolon@users.noreply.github.com>
2023-01-07 20:52:54 -0500

    Implemented Code for Fire

---
+++ Assets/Scripts/Core/Combat/FireActor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Mechanizer
+{
+    public class FireActor : Attack
+    {
+        private Coroutine _routine;
+        [Header("Fire Attributes")]
+        [SerializeField] private float _spawnSpeed;
+        [SerializeField] private float _despawnSpeed;
+        [SerializeField] private float _tickRate;
+        [SerializeField] private float _duration;
+        private void Start()
+        {
+            DoRoutine(SpawnRoutine());
+        }
+
+        private void DoRoutine(IEnumerator newRoutine)
+        {
+            if (_routine != null)
+                StopCoroutine(_routine);
+            _routine = StartCoroutine(newRoutine);
+        }
+
+        private IEnumerator SpawnRoutine()
+        {
+            float spawnTime = 0f;
+            while (spawnTime < 1.0f)
+            {
+                spawnTime += Time.deltaTime * _spawnSpeed;
+                Vector3 scale = Vector3.Lerp(Vector3.zero, Vector3.one, spawnTime);
+                transform.localScale = scale;
+                yield return null;
+            }
+
+            DoRoutine(TickRoutine());
+        }
+
+        private IEnumerator TickRoutine()
+        {
+            float tickTime = 0f;
+            float time = 0f;
+            while(time < _duration)
+            {
+                tickTime += Time.deltaTime;
+                time += Time.deltaTime;
+                if (tickTime >= _tickRate)
+                {
+                    Perform();
+                    tickTime = 0f;
+                }
+                yield return null;
+            }
+
+            DoRoutine(DeSpawnRoutine());
+        }
+
+        private IEnumerator DeSpawnRoutine()
+        {
+            float spawnTime = 0f;
+            while (spawnTime < 1.0f)
+            {
+                spawnTime += Time.deltaTime * _despawnSpeed;
+                Vector3 scale = Vector3.Lerp(Vector3.one, Vector3.zero, spawnTime);
+                transform.localScale = scale;
+                yield return null;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Combat/FireBurster.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Mechanizer
+{
+    [RequireComponent(typeof(Throwable))]
+    public class FireBurster : MonoBehaviour
+    {
+        private Throwable _throwable;
+        [SerializeField] private FireActor _firePrefab;
+        [SerializeField] private float _blastRadius;
+        [SerializeField] private int _minFireCount;
+        [SerializeField] private int _maxFireCount;
+        private void OnEnable()
+        {
+            if (_throwable == null)
+                _throwable = GetComponent<Throwable>();
+            _throwable.OnDeath += CreateFire;
+        }
+        private void OnDisable()
+        {
+            _throwable.OnDeath -= CreateFire;
+        }
+
+        private void CreateFire()
+        {
+            int fireCount = Random.Range(_minFireCount, _maxFireCount);
+            for (int i = 0; i < fireCount; i++)
+            {
+                float x = Random.Range(-_blastRadius, _blastRadius);
+                float y = Random.Range(-_blastRadius, _blastRadius);
+                Vector3 offset = new Vector3(x, y, 0);
+                Vector3 fireSpawnPoint = transform.position + offset;
+                Instantiate(_firePrefab, fireSpawnPoint, _firePrefab.transform.rotation);
+            }
+        }
+    }
+}
