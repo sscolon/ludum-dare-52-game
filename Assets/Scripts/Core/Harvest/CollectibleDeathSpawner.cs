cb24037ec1106c9d997e89fada9396183c231f24
Scolon <49799704+sscolon@users.noreply.github.com>
2023-01-08 13:44:34 -0500

    Collectibles can no longer spawn out of bounds

--- Assets/Scripts/Core/Harvest/CollectibleDeathSpawner.cs
+++ Assets/Scripts/Core/Harvest/CollectibleDeathSpawner.cs
@@ -25,13 +25,20 @@ namespace Mechanizer
         private void SpawnCollectibles()
         {
             int count = Random.Range(_min, _max);
-            for(int i = 0; i < count; i++)
+            LayerMask mask = LayerMask.GetMask("Wall");
+            for (int i = 0; i < count; i++)
             {
                 Collectible collectible = _collectibleData.NextCollectible(GameManager.Main.Rand);
                 float x = Random.Range(-_radius, _radius);
                 float y = Random.Range(-_radius, _radius);
                 Vector3 offset = new Vector3(x, y, 0);
                 Vector3 position = transform.position + offset;
+                var hit = Physics2D.Raycast(transform.position, (position - transform.position).normalized, Vector3.Distance(transform.position, position), mask);
+                if (hit)
+                {
+                    position = hit.point;
+                }
+                
                 Instantiate(collectible, position, collectible.transform.rotation);
             }
         }
