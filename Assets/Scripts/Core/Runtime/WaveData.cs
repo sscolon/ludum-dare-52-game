7ca36c1696561b1d8021ca66a09e06e44863621b
Scolon <49799704+sscolon@users.noreply.github.com>
2023-01-08 14:04:53 -0500

    WEIRD BUGG

--- Assets/Scripts/Core/Runtime/WaveData.cs
+++ Assets/Scripts/Core/Runtime/WaveData.cs
@@ -6,18 +6,7 @@ namespace Mechanizer
     [CreateAssetMenu(menuName = "Mechanizer/Wave Data")]
     public class WaveData : ScriptableObject
     {
-        private Wave[] _waves;
-
         [SerializeField] private List<WaveContainer> _waveContainers;
-        public void Init()
-        {
-            _waves = new Wave[_waveContainers.Count];
-            for (int i = 0; i < _waveContainers.Count; i++)
-            {
-                _waves[i] = _waveContainers[i].Wave;
-            }
-        }
-
         public void AddWaveContainer(WaveContainer container)
         {
             _waveContainers.Add(container);
@@ -26,9 +15,9 @@ namespace Mechanizer
         public Wave NextWave(Rand rand, int difficulty)
         {
             List<Wave> waves = new List<Wave>();
-            for(int i = 0; i < _waves.Length; i++)
+            for(int i = 0; i < _waveContainers.Count; i++)
             {
-                Wave w = _waves[i];
+                Wave w = _waveContainers[i].Wave;
                 if (w.difficulty <= difficulty)
                     waves.Add(w);
             }
