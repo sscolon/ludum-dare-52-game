7388801dbde182f8905a48ae99c319864bb42416
Scolon <49799704+sscolon@users.noreply.github.com>
2023-01-08 12:50:25 -0500

    Added Iris Themes

---
+++ Assets/Scripts/Sound/MusicPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Mechanizer
+{
+    public class MusicPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioClip _musicClip;
+        private void Start()
+        {
+            SoundSettings settings = SoundSettings.MainMusic;
+            settings.clip = _musicClip;
+            SoundManager.PlayMainMusic(_musicClip);
+        }
+    }
+}
